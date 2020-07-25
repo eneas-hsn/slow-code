4170e15c933ad3fe4053d4290750f416d1a2aa7c
eneas-hsn <eneashsn@gmail.com>
2020-07-25 16:48:58 -0300

    Create script.js

---
+++ DOM/script.js
@@ -0,0 +1,14 @@
+
+const botao = window.document.getElementById('btn')
+botao.innerText = 'Manipulando o DOM'
+
+function ativar() {
+    botao.style.backgroundColor = 'blue'
+    botao.style.margin = '100px'
+    
+}
+
+
+
+
+

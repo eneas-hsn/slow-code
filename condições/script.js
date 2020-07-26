b2d4ff5b1910aadd2898736e0da79d43ee268376
eneas-hsn <eneashsn@gmail.com>
2020-07-25 22:49:28 -0300

    Update script.js

---
+++ condições/script.js
@@ -0,0 +1,14 @@
+
+const usuario={
+    nome:"Jorge",
+    idade:17
+}
+if (usuario.idade>=18) {
+    console.log(`${usuario.nome} é maior de idade`)
+} else{
+    console.log(`${usuario.nome} é menor de idade`)
+}
+
+//Console vai escrever:
+//Jorge é menor da idade
+
